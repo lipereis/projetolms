dc598658a98ce07df2f2ebd60aa012fe511d4542
lipereis <felipedrg02@gmail.com>
2025-08-28 22:08:34 -0300

    Delete projeto-lms directory

--- projeto-lms/0707-trigger/comandos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TRIGGER "set_users_updated"
-AFTER UPDATE ON "users"
-BEGIN
-  UPDATE "users" SET "updated" = CURRENT_TIMESTAMP WHERE "id" = NEW."id";
-END;
-
-UPDATE "users" SET "email" = 'renata5@email.com' WHERE "id" = 9;
--- projeto-lms/0708-delete/comandos.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM "sessions" WHERE "token" = 'DBBE430A92F4CCAD';
-DELETE FROM "users" WHERE "id" = 10;
-
-DELETE FROM "sessions" WHERE "expires" < STRFTIME('%s','now');
